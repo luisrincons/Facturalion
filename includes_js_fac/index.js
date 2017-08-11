aa5d69e02260387e219a8a3f97ba1e6d7d3f44a2
luisrincons <luisrincons@gmail.com>
2017-08-10 19:33:07 -0500

    v1.0.6

--- includes_js_fac/index.js
+++ includes_js_fac/index.js
@@ -20,16 +20,31 @@ $(document).ready(function() {
 		  window.location.href = "index_desktop.html";
 		}
 		return;	
-	}).fail(function(xhr, textStatus, errorThrown) {
-	  //alert(errorThrown);
-	  $.spin('false');
-	  new PNotify({
-		  title: 'Error en la conexión',
-		  text: errorThrown,
-		  type: 'error',
-		  delay: 1000
-	  });		  
-	  return;	  
+	}).fail(function(jqXHR, textStatus, errorThrown) {
+		var code_error = "";
+        if (jqXHR.status === 0) {
+           code_error = 'Not connect: Verify Network.';
+        } else if (jqXHR.status == 404) {
+           code_error = 'Requested page not found [404]';
+        } else if (jqXHR.status == 500) {
+           code_error = 'Internal Server Error [500].';
+        } else if (textStatus === 'parsererror') {
+           code_error = 'Requested JSON parse failed.';
+        } else if (textStatus === 'timeout') {
+           code_error = 'Time out error.';
+        } else if (textStatus === 'abort') {
+           code_error = 'Ajax request aborted.';
+        } else {
+           code_error = 'Uncaught Error: ' + jqXHR.responseText;
+        }
+		$.spin('false');
+		new PNotify({
+		    title: code_error,
+			text: errorThrown,
+			type: 'error',
+			delay: 2500
+		});		  
+		return;	  
 	}).always(function() {
 	   $.spin('false');
 	});	
